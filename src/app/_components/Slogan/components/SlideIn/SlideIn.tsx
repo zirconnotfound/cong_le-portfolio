abcdeb196c4615cc5cf8a7aa3880f13bb31d0d8e
Cong Son <congson7125@gmail.com>
2025-09-24 00:26:31 +0700

    minor changes to effect

--- src/app/_components/Slogan/components/SlideIn/SlideIn.tsx
+++ src/app/_components/Slogan/components/SlideIn/SlideIn.tsx
@@ -14,19 +14,19 @@ const SlideIn = ({ firstLine, secondLine }: SlideInProps) => {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["start 1.3", "start 0"],
+    offset: ["start 1.3", "start 0.2"],
   });
 
   const x1 = useTransform(scrollYProgress, [0, 1], ["-100%", "0%"]);
   const x2 = useTransform(scrollYProgress, [0, 1], ["100%", "0%"]);
   return (
     <div ref={ref} className={`${styles["container"]} ${sfuCentury.className}`}>
-      <motion.h1 style={{ x: x1 }} className={styles["line"]}>
-        {firstLine}
-      </motion.h1>
-      <motion.h1 style={{ x: x2 }} className={styles["line"]}>
-        {secondLine}
-      </motion.h1>
+      <div className={`${styles["line"]} ${styles["first-line"]}`}>
+        <motion.h1 style={{ x: x1 }}>{firstLine}</motion.h1>
+      </div>
+      <div className={`${styles["line"]} ${styles["second-line"]}`}>
+        <motion.h1 style={{ x: x2 }}>{secondLine}</motion.h1>
+      </div>
     </div>
   );
 };
